cd35adfd37ba1e47f6ebffcfda88b7fcbf97ea09
kampron <qwequkyei@gmail.com>
2023-06-20 10:05:05 +0000

    server

--- server.js
+++ server.js
@@ -27,8 +27,9 @@ app.use('/quizonnet/questions', englishShs)
 app.use('/quizonnet/results', result)
 
 // connet to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
+        // listen for requests
         app.listen(process.env.PORT, () => {
             console.log('connected to db & listening on port', process.env.PORT)
         })
