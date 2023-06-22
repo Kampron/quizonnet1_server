897ecf3c4059f05703ff0abbfabea39bd2ba0168
kampron <qwequkyei@gmail.com>
2023-06-22 06:27:41 +0000

    cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/userRoutes')
 const englishShs = require('./routes/englishShsRoutes')
@@ -15,7 +15,7 @@ const app = express()
 app.set("view engine","ejs");
 // middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use((req, res, next) => {
